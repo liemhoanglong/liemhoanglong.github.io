360a8558c6919f6a50a2942ccfd8545f20ea9352
liemhoanglong@gmail.com <liemhoanglong@gmail.com>
2019-08-14 15:42:16 +0700

    add html and css to skills & hobbies content

--- 1.js
+++ 1.js
@@ -1,4 +1,13 @@
 ﻿ $(function(){
- 
+ 	vitricuaedu = $('#khoi_edu').offset().top;
+ 	$(".xuong,.exp").click(function(){
+ 		$('body,html').animate({scrollTop:vitricuaedu - 40}, 700)
+ 	});
+
+ 	vitricuaskills = $('#khoi_skills').offset().top;
+ 	$(".xuong,.skills").click(function(){
+ 		$('body,html').animate({scrollTop:vitricuaskills}, 700)
+ 	});
+
 })  
  
